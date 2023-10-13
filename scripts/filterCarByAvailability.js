4167cb608141b231eebd77f54d5ce245b96acd3e
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2023-10-13 11:13:39 +0700

    clean comment

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -1,9 +1,3 @@
-/**
- *
- * @param {Array} cars
- * @returns
- */
-
 function filterCarByAvailability(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
   console.log(cars);
@@ -11,22 +5,9 @@ function filterCarByAvailability(cars) {
   // Tempat penampungan hasil
   let result = [];
 
-  // Tulis code-mu disini
-  // for (let i = 0; i < cars.length; i++) {
-  //   if (cars[i].available === true) {
-  //     result.push(cars[i]);
-  //   }
-  // }
-
   cars.forEach((item) => {
-    item.available === true ? result.push(item) : "Ignore";
+    if (item.available === true) result.push(item);
   });
-
-  // for (const item of cars) {
-  //   item.available === true ? result.push(item) : "ignore";
-  // }
-
-  console.log(result);
 
   // Rubah code ini dengan array hasil filter berdasarkan availablity
   return result;
--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -12,24 +12,17 @@ function sortCarByYearAscendingly(cars) {
   let swapValue = true;
 
   while (swapValue) {
-    swapValue = false; // berubah swap value
+    swapValue = false;
 
     for (let i = 0; i < result.length - 1; i++) {
-      //length 24 , index 0-23
       if (result[i].year > result[i + 1].year) {
-        //[0].year > [1]. year
-        // 1.year > 2.year (2021)
-        // 2022 > 3.year (2018)
-
         swapValue = true;
-        const temp = result[i]; // 2022, 2022 , 2022
-        result[i] = result[i + 1]; // 2022 => 2019 , 2022 => 2021, 2022 => 2018
-        result[i + 1] = temp; // 2019 => 2022 , 2021 ,2022, 2018 => 2022
+        const temp = result[i];
+        result[i] = result[i + 1];
+        result[i + 1] = temp;
       }
     }
   }
-
-  //urutan by tahun 2019 2021 2018 2022
 
   console.log(result);
 
